e3edcaf7fb231a681079e08964fed56ecff94748
Tuan Anh <ntanh0794@gmail.com>
2019-10-14 16:17:00 +0700

    finish basic tutorial ts

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
+"use strict";
 function greeter(person) {
     var sayHi = "Hello " + person;
     console.log(sayHi);
 }
 greeter('tuan anh');
+// Type of ts
+var a;
+var b;
+var c;
+var d = ['a', 'b', 'c']; // boolean[], number[], any[]
+var e; // default type of variable
+var f;
+var g;
+var h; // funcion method
+var Color;
+(function (Color) {
+    Color["Red"] = "red";
+    Color["Green"] = "green";
+    Color["Yellow"] = "yellow";
+})(Color || (Color = {}));
+// example for enum
+var red = Color.Red;
+//custom type ?
+// TypeAsertion
+var ta;
+// ta. show all avaible method in string[]
+// Cast
+var test = ['ta'];
+var eat = function (food) {
+    console.log("eat " + food.x + ", " + food.y);
+};
+var makeFood = function (food) {
+};
+//Conhesion things related to each other should stay in same place ? => solution use Class
+//Class
+var Foods = /** @class */ (function () {
+    // access modifiers: public, private, protected
+    // default access modifiers is public
+    function Foods(x, y) {
+        var _this = this;
+        this.makeFood = function () {
+            console.log("make food " + _this.x + ", " + _this.y);
+        };
+        this.x = x;
+        this.y = y;
+    }
+    return Foods;
+}());
+var newfood = new Foods('rice', 'meat');
+newfood.makeFood();
--- main.ts
+++ main.ts
@@ -5,12 +5,12 @@ function greeter(person: string) {
 
 greeter('tuan anh')
 
-// type of ts
+// Type of ts
 
 let a: string;
 let b: number;
 let c: boolean;
-let d: string[] = ['a', 'b', 'c']; // boolean[], number[]
+let d: string[] = ['a', 'b', 'c']; // boolean[], number[], any[]
 let e: any; // default type of variable
 let f: null;
 let g: undefined;
@@ -24,3 +24,75 @@ enum Color {
 
 // example for enum
 let red = Color.Red
+
+//custom type ?
+
+
+// TypeAsertion
+let ta: string[];
+
+// ta. show all avaible method in string[]
+
+// Cast
+let test = ['ta'];
+// (<string[]>test) cast 1 
+// (test as string[]) cast 2
+
+// error 
+// completion error show on VS code
+// runtime error show after comple
+
+
+// Interface
+
+interface Food {
+    x: string;
+    y: string;
+}
+
+let eat = (food: Food) => {
+    console.log(`eat ${food.x}, ${food.y}`)
+}
+
+let makeFood = (food: Food) => {
+
+}
+
+
+//Conhesion things related to each other should stay in same place ? => solution use Class
+
+//Class
+
+//Getter setter
+
+class Foods {
+    // access modifiers: public, private, protected
+    // default access modifiers is public
+
+    constructor(private _x: string, private _y: string) {
+        this._x = _x
+        this._y = _y
+    }
+
+    makeFood = () => {
+        console.log(`make food ${this._x}, ${this._y}`)
+    }
+
+    get x() {
+        return this._x
+    }
+    
+    set x(value: string) {
+        if (!value) {
+            throw new Error('cannot set empty string value to this variable')
+        }
+        this._x = value
+    }
+    
+}
+
+let newfood = new Foods('rice', 'meat')
+console.log(newfood.x)
+newfood.x = 'raw meat'
+console.log(newfood.x)
+newfood.makeFood()
